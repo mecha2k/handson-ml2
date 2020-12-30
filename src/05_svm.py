98be0d3fa04ca396702a3063b21ecf3c2b852bd4
mecha2k <mecha2k@naver.com>
2020-12-31 06:37:12 +0900

    init mac

---
+++ src/05_svm.py
@@ -0,0 +1,206 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.svm import SVC
+from sklearn import datasets
+
+iris = datasets.load_iris()
+X = iris["data"][:, (2, 3)]  # petal length, petal width
+y = iris["target"]
+
+setosa_or_versicolor = (y == 0) | (y == 1)
+X = X[setosa_or_versicolor]
+y = y[setosa_or_versicolor]
+
+# SVM Classifier model
+svm_clf = SVC(kernel="linear", C=float("inf"))
+svm_clf.fit(X, y)
+
+# Bad models
+x0 = np.linspace(0, 5.5, 200)
+pred_1 = 5 * x0 - 20
+pred_2 = x0 - 1.8
+pred_3 = 0.1 * x0 + 0.5
+
+
+def plot_svc_decision_boundary(svm_clf_, xmin, xmax):
+    w = svm_clf_.coef_[0]
+    b = svm_clf_.intercept_[0]
+
+    # At the decision boundary, w0*x0 + w1*x1 + b = 0
+    # => x1 = -w0/w1 * x0 - b/w1
+    x0_ = np.linspace(xmin, xmax, 200)
+    decision_boundary = -w[0] / w[1] * x0_ - b / w[1]
+
+    margin = 1 / w[1]
+    gutter_up = decision_boundary + margin
+    gutter_down = decision_boundary - margin
+
+    svs = svm_clf_.support_vectors_
+    plt.scatter(svs[:, 0], svs[:, 1], s=180, facecolors="#FFAAAA")
+    plt.plot(x0_, decision_boundary, "k-", linewidth=2)
+    plt.plot(x0_, gutter_up, "k--", linewidth=2)
+    plt.plot(x0_, gutter_down, "k--", linewidth=2)
+
+
+fig, axes = plt.subplots(ncols=2, figsize=(10, 4), sharey=True)
+
+plt.sca(axes[0])
+plt.plot(x0, pred_1, "g--", linewidth=2)
+plt.plot(x0, pred_2, "m-", linewidth=2)
+plt.plot(x0, pred_3, "r-", linewidth=2)
+plt.plot(X[:, 0][y == 1], X[:, 1][y == 1], "bs", label="Iris versicolor")
+plt.plot(X[:, 0][y == 0], X[:, 1][y == 0], "yo", label="Iris setosa")
+plt.xlabel("Petal length", fontsize=14)
+plt.ylabel("Petal width", fontsize=14)
+plt.legend(loc="upper left", fontsize=14)
+plt.axis([0, 5.5, 0, 2])
+
+plt.sca(axes[1])
+plot_svc_decision_boundary(svm_clf, 0, 5.5)
+plt.plot(X[:, 0][y == 1], X[:, 1][y == 1], "bs")
+plt.plot(X[:, 0][y == 0], X[:, 1][y == 0], "yo")
+plt.xlabel("Petal length", fontsize=14)
+plt.axis([0, 5.5, 0, 2])
+plt.show()
+
+
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import LinearSVC
+
+
+scaler = StandardScaler()
+svm_clf1 = LinearSVC(C=1, loss="hinge", random_state=42)
+svm_clf2 = LinearSVC(C=100, loss="hinge", random_state=42)
+
+scaled_svm_clf1 = Pipeline(
+    [
+        ("scaler", scaler),
+        ("linear_svc", svm_clf1),
+    ]
+)
+scaled_svm_clf2 = Pipeline(
+    [
+        ("scaler", scaler),
+        ("linear_svc", svm_clf2),
+    ]
+)
+
+scaled_svm_clf1.fit(X, y)
+scaled_svm_clf2.fit(X, y)
+
+# Convert to unscaled parameters
+b1 = svm_clf1.decision_function([-scaler.mean_ / scaler.scale_])
+b2 = svm_clf2.decision_function([-scaler.mean_ / scaler.scale_])
+w1 = svm_clf1.coef_[0] / scaler.scale_
+w2 = svm_clf2.coef_[0] / scaler.scale_
+svm_clf1.intercept_ = np.array([b1])
+svm_clf2.intercept_ = np.array([b2])
+svm_clf1.coef_ = np.array([w1])
+svm_clf2.coef_ = np.array([w2])
+
+# Find support vectors (LinearSVC does not do this automatically)
+t = y * 2 - 1
+support_vectors_idx1 = (t * (X.dot(w1) + b1) < 1).ravel()
+support_vectors_idx2 = (t * (X.dot(w2) + b2) < 1).ravel()
+svm_clf1.support_vectors_ = X[support_vectors_idx1]
+svm_clf2.support_vectors_ = X[support_vectors_idx2]
+
+
+_, axes = plt.subplots(ncols=2, figsize=(10, 2.7), sharey=True)
+
+plt.sca(axes[0])
+plt.plot(X[:, 0][y == 1], X[:, 1][y == 1], "g^", label="Iris virginica")
+plt.plot(X[:, 0][y == 0], X[:, 1][y == 0], "bs", label="Iris versicolor")
+plot_svc_decision_boundary(svm_clf1, 4, 5.9)
+plt.xlabel("Petal length", fontsize=14)
+plt.ylabel("Petal width", fontsize=14)
+plt.legend(loc="upper left", fontsize=14)
+plt.title("$C = {}$".format(svm_clf1.C), fontsize=16)
+plt.axis([4, 5.9, 0.8, 2.8])
+
+plt.sca(axes[1])
+plt.plot(X[:, 0][y == 1], X[:, 1][y == 1], "g^")
+plt.plot(X[:, 0][y == 0], X[:, 1][y == 0], "bs")
+plot_svc_decision_boundary(svm_clf2, 4, 5.99)
+plt.xlabel("Petal length", fontsize=14)
+plt.title("$C = {}$".format(svm_clf2.C), fontsize=16)
+plt.axis([4, 5.9, 0.8, 2.8])
+plt.show()
+
+
+from sklearn.datasets import make_moons
+
+
+def plot_dataset(X_, y_, axes_):
+    plt.plot(X_[:, 0][y_ == 0], X_[:, 1][y_ == 0], "bs")
+    plt.plot(X_[:, 0][y_ == 1], X_[:, 1][y_ == 1], "g^")
+    plt.axis(axes_)
+    plt.grid(True, which="both")
+    plt.xlabel(r"$x_1$", fontsize=20)
+    plt.ylabel(r"$x_2$", fontsize=20, rotation=0)
+
+
+X, y = make_moons(n_samples=100, noise=0.15, random_state=42)
+
+plot_dataset(X, y, [-1.5, 2.5, -1, 1.5])
+plt.show()
+
+from sklearn.datasets import make_moons
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures
+
+polynomial_svm_clf = Pipeline(
+    [
+        ("poly_features", PolynomialFeatures(degree=3)),
+        ("scaler", StandardScaler()),
+        ("svm_clf", LinearSVC(C=10, loss="hinge", random_state=42, max_iter=2000)),
+    ]
+)
+
+polynomial_svm_clf.fit(X, y)
+
+cmaps = plt.colormaps()
+for cm in cmaps:
+    print(cm)
+
+
+def plot_predictions(clf, axes_):
+    x0s = np.linspace(axes_[0], axes_[1], 100)
+    x1s = np.linspace(axes_[2], axes_[3], 100)
+    x0_, x1 = np.meshgrid(x0s, x1s)
+    X_ = np.c_[x0_.ravel(), x1.ravel()]
+    y_pred = clf.predict(X_).reshape(x0_.shape)
+    y_decision = clf.decision_function(X_).reshape(x0_.shape)
+    plt.contourf(x0_, x1, y_pred, cmap=plt.cm.brg, alpha=0.2)
+    plt.contourf(x0_, x1, y_decision, cmap=plt.cm.brg, alpha=0.1)
+
+
+plot_predictions(polynomial_svm_clf, [-1.5, 2.5, -1, 1.5])
+plot_dataset(X, y, [-1.5, 2.5, -1, 1.5])
+plt.show()
+
+poly_kernel_svm_clf = Pipeline(
+    [("scaler", StandardScaler()), ("svm_clf", SVC(kernel="poly", degree=3, coef0=1, C=5))]
+)
+poly_kernel_svm_clf.fit(X, y)
+
+poly100_kernel_svm_clf = Pipeline(
+    [("scaler", StandardScaler()), ("svm_clf", SVC(kernel="poly", degree=10, coef0=100, C=5))]
+)
+poly100_kernel_svm_clf.fit(X, y)
+
+fig, axes = plt.subplots(ncols=2, figsize=(10.5, 4), sharey=True)
+
+plt.sca(axes[0])
+plot_predictions(poly_kernel_svm_clf, [-1.5, 2.45, -1, 1.5])
+plot_dataset(X, y, [-1.5, 2.4, -1, 1.5])
+plt.title(r"$d=3, r=1, C=5$", fontsize=18)
+
+plt.sca(axes[1])
+plot_predictions(poly100_kernel_svm_clf, [-1.5, 2.45, -1, 1.5])
+plot_dataset(X, y, [-1.5, 2.4, -1, 1.5])
+plt.title(r"$d=10, r=100, C=5$", fontsize=18)
+plt.ylabel("")
+plt.show()
